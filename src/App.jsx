ff35abbf655f5c8c5882747fd117ae4f0e4fc72f
RohitR27 <khajindar.rohit7@gmail.com>
2026-02-05 18:03:39 +0530

    day 01 work

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
 
 const App = () => {
+  const [counter, setCounter] = useState (0);
+
+
   return (
     <div>
-      <h1 className='underline font-bold text-red-500'>Hello!</h1>
+      <h1 className='text-3xl font-bold  text-red-500'>{counter}</h1>
+      <button className='border border-slate-700 rounded-sm' onClick={() => setCounter (counter + 1)}>
+        Click Me
+      </button>
+      <button className='ml-2 border border-slate-700 rounded-sm' onClick={() => setCounter(0)}>  Reset </button>
+
     </div>
   )
 }
